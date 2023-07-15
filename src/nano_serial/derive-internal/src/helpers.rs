5acf216ee40c2a10bad998850dd8849ff1676720
krishpranav <krisna.pranav@gmail.com>
2023-07-15 11:52:47 +0530

    helpers + lib

--- src/nano_serial/derive-internal/src/helpers.rs
+++ src/nano_serial/derive-internal/src/helpers.rs
@@ -12,11 +12,11 @@ pub fn contains_initialize_with(attrs: &[Attribute]) -> Option<Path> {
     for attr in attrs.iter() {
         if attr.path().is_ident("init_serialize") {
             let mut res = None;
-
             let _ = attr.parse_nested_meta(|meta| {
                 res = Some(meta.path);
                 Ok(())
             });
+            return res
         }
     }
 
--- src/nano_serial/derive-internal/src/lib.rs
+++ src/nano_serial/derive-internal/src/lib.rs
@@ -1,1 +1,25 @@
+use std::collections::HashMap;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote;
+use syn::{
+    punctuated::Punctuated, token::Comma, Fields, FieldsNamed, Index, FieldsUnnamed, ItemEnum,
+    ItemStruct, Variant, WhereClause, WherePredicate,
+};
 mod helpers;
+use helpers::{contains_initialize_with, contains_skip};
+
+struct VariantParts {
+    where_predicates: Vec<WherePredicate>,
+    variant_headers: TokenStream,
+    variant_body: TokenStream,
+    variant_idx_body: TokenStream,
+}
+
+fn discriminant_map(variants: &Punctuated<Variant, Comma>) -> HashMap<Ident, TokenStream> {
+    let mut map = HashMap::new();
+
+    let mut next_discrimant_if_not_specified = quote! {0};
+
+    for variant in variants {
+    }
+}
